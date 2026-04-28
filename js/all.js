aefb185f3ddb58a0d511580cd3b57b5e7a8f7adb
CONG-REN WANG <116715193+LockingWang@users.noreply.github.com>
2026-04-28 12:48:15 +0800

    Update index.html and CSS styles; add pet memorial section and responsive design improvements

--- js/all.js
+++ js/all.js
@@ -1,4 +1,38 @@
 $(document).ready(function () {
+    /** 依 YYYY-MM-DD（本地時區）計算足歲（截至指定「參考日」，含過世日） */
+    function petAgeYearsAsOf(birthStr, refStr) {
+        const pb = birthStr.split('-').map(Number);
+        const pr = refStr.split('-').map(Number);
+        if (pb.length !== 3 || pr.length !== 3 || pb.some(Number.isNaN) || pr.some(Number.isNaN)) {
+            return '';
+        }
+        const birth = new Date(pb[0], pb[1] - 1, pb[2]);
+        const ref = new Date(pr[0], pr[1] - 1, pr[2]);
+        let age = ref.getFullYear() - birth.getFullYear();
+        const monthDiff = ref.getMonth() - birth.getMonth();
+        if (monthDiff < 0 || (monthDiff === 0 && ref.getDate() < birth.getDate())) {
+            age--;
+        }
+        return age;
+    }
+    /** 依生日計算目前足歲 */
+    function petAgeYears(birthStr) {
+        const y = new Date().getFullYear();
+        const m = String(new Date().getMonth() + 1).padStart(2, '0');
+        const d = String(new Date().getDate()).padStart(2, '0');
+        return petAgeYearsAsOf(birthStr, y + '-' + m + '-' + d);
+    }
+    $('.pet-age').each(function () {
+        const $el = $(this);
+        const birth = $el.attr('data-birth');
+        const passed = $el.attr('data-passed');
+        if (passed) {
+            $el.text(petAgeYearsAsOf(birth, passed));
+        } else {
+            $el.text(petAgeYears(birth));
+        }
+    });
+
     // 漢堡選單
     $('.mobile-menuBTN').click(function (e) { 
         e.preventDefault();
@@ -53,7 +87,7 @@ $(document).ready(function () {
 
         // 滾動效果(耗效能)
         $('.placeIntro img').css('transform', 'translateX( ' + -(scrollPos / 2) + 'px )')
-        $('.intro_money, .intro_marbo').css('background-position-x', (scrollPos / 2) +'px')
+        $('.intro_money, .intro_marbo, .intro_mumu').css('background-position-x', (scrollPos / 2) +'px')
         $('.intro_mi').css('background-position-x', -(scrollPos / 2) +'px')
     })
 });
